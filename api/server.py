b8d407391a975318e756fd1eeb0098b99d32f749
Sean Buckley <sean.bck@gmail.com>
2020-02-23 01:13:50 -0500

    lets get async

--- api/server.py
+++ api/server.py
@@ -1,43 +1,47 @@
 #!/usr/bin/env python3
 
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from itertools import groupby
 from time import time
 from secrets import token_hex
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from quart import Quart, jsonify, request
+from quart_cors import cors
 
-app = Flask(__name__)
-CORS(app)
-db = MongoClient('mongodb://127.0.0.1:27017')['hca']
+app = Quart(__name__)
+cors(app)
 
 
-def get_session_username():
+@app.before_first_request
+async def start_db():
+    global db
+    db = AsyncIOMotorClient('mongodb://127.0.0.1:27017')['hca']
+
+
+async def get_session_username():
     if 'X-Sesid' in request.headers:
-        r = db.sessions.find_one({'sesid': request.headers['X-Sesid']})
+        r = await db.sessions.find_one({'sesid': request.headers['X-Sesid']})
         if r:
             return r['username']
     return None
 
 
-def all_challenges(username):
-    def challengeSolveCount(chal):
-        chal['solves'] = db.solves.count_documents(
-            {'challenge': chal['title']})
-        return chal
+async def all_challenges(username):
+    chals = await db.challenges.find(
+        {}, {'_id': 0, 'flag': 0}
+    ).to_list(length=None)
 
-    def checkIfUserSolved(chal):
-        chal['solved'] = bool(
-            db.solves.find_one({'username': username,
-                                'challenge': chal['title'], }))
-        return chal
+    for c in chals:
+        c['solves'] = await db.solves.count_documents(
+            {'challenge': c['title']})
 
-    return map(checkIfUserSolved,
-               map(challengeSolveCount,
-                   db.challenges.find({}, {'_id': 0, 'flag': 0})))
+    for c in chals:
+        c['solved'] = bool(
+            await db.solves.find_one({'username': username,
+                                      'challenge': c['title'], }))
+    return chals
 
 
-def all_challenges_grouped(username):
+async def all_challenges_grouped(username):
     def getCat(x):
         return x['category']
 
@@ -47,88 +51,91 @@ def all_challenges_grouped(username):
 
     return [
         {'title': category, 'chals': list(map(removeCat, chals))}
-        for category, chals
-        in groupby(sorted(all_challenges(username), key=getCat), key=getCat)
+        for category, chals in
+        groupby(sorted(await all_challenges(username), key=getCat), key=getCat)
     ]
 
 
-def getUserScore(username):
+async def getUserScore(username):
     return sum([
-        db.challenges.find_one({'title': x['challenge']})['points']
-        for x in db.solves.find({'username': username})
+        (await db.challenges.find_one({'title': x['challenge']}))['points']
+        for x
+        in await db.solves.find({'username': username}).to_list(length=None)
     ])
 
 
 @app.route("/login", methods=['POST'])
-def login():
-    args = request.get_json(force=True)
-    if not db.users.find_one({'username': args['username']}):
+async def login():
+    args = await request.get_json(force=True)
+    if not await db.users.find_one({'username': args['username']}):
         return jsonify(False)
     t = token_hex()
-    db.sessions.insert_one({'username': args['username'], 'sesid': t})
+    await db.sessions.insert_one({'username': args['username'], 'sesid': t})
     return jsonify({"sesid": t})
 
 
 @app.route("/logout")
-def logout():
+async def logout():
     sesid = request.headers['X-Sesid']
     return jsonify({
-        'ok': bool(db.sessions.delete_one({'sesid': sesid}).deleted_count)
+        'ok': bool(
+            (await db.sessions.delete_one({'sesid': sesid})).deleted_count
+        )
     })
 
 
 @app.route("/userinfo")
-def userinfo():
-    username = get_session_username()
+async def userinfo():
+    username = await get_session_username()
     if not username:
         return jsonify(False)
-    u = db.users.find_one({'username': username}, {'_id': 0,
-                                                   'email': 1,
-                                                   'username': 1, })
-    u['score'] = getUserScore(u['username'])
+    u = await db.users.find_one({'username': username}, {'_id': 0,
+                                                         'email': 1,
+                                                         'username': 1, })
+    u['score'] = await getUserScore(u['username'])
     return jsonify(u) if u else jsonify(False)
 
 
 @app.route("/submitflag", methods=['POST'])
-def submitflag():
-    username = get_session_username()
+async def submitflag():
+    username = await get_session_username()
     assert username
     args = request.get_json(force=True)
-    c = db.challenges.find_one({'flag': args['flag']})
+    c = await db.challenges.find_one({'flag': args['flag']})
     if not c:
         return jsonify({'ok': False, 'msg': "Unknown flag."})
 
     entry = {'username': username, 'challenge': c['title']}
 
-    if db.solves.find_one(entry):
+    if await db.solves.find_one(entry):
         return jsonify({'ok': False, 'msg': "You've already solved that one."})
 
-    db.solves.insert_one(entry)
+    await db.solves.insert_one(entry)
 
-    db.users.update_one({'username': username},
-                        {"$set": {"lastSolveTime": int(time())}})
+    await db.users.update_one({'username': username},
+                              {"$set": {"lastSolveTime": int(time())}})
 
     return jsonify({'ok': True, 'msg': 'Nice job!'})
 
 
 @app.route("/scoreboard")
-def scoreboard():
+async def scoreboard():
     return jsonify([
         [x[2], x[0]] for x in
         sorted([
             [
-                getUserScore(u['username']),
+                await getUserScore(u['username']),
                 0-u['lastSolveTime'],
                 u['username'],
             ]
-            for u in db.users.find()
+            for u in await db.users.find().to_list(length=None)
         ], reverse=True)
     ])
 
 
 @app.route("/challenges")
-def challenges():
-    return jsonify(all_challenges_grouped(get_session_username()))
+async def challenges():
+    return jsonify(await all_challenges_grouped(await get_session_username()))
 
 
 if __name__ == '__main__':
