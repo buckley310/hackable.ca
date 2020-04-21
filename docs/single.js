acc9917fc058f3ad5f25338c3b55ec9be3830e09
Sean Buckley <sean.bck@gmail.com>
2020-04-21 01:43:08 -0400

    docs

--- docs/single.js
+++ docs/single.js
@@ -6,12 +6,20 @@ function loadingError(e) {
 }
 
 function api(path, args = false) {
+    let headers = {};
+
     let st = localStorage.getItem('sessionToken');
+    if (st)
+        headers['X-Sesid'] = st;
+
+    if (args)
+        headers['Content-Type'] = 'application/json';
+
     return fetch('http://127.0.0.1:5000/' + path, {
         cache: "no-cache",
         method: args ? 'POST' : 'GET',
         body: args ? JSON.stringify(args) : null,
-        headers: st ? { 'X-Sesid': st } : {}
+        headers: headers
     }).then(x => x.json());
 }
 
