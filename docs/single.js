63c7bd2b43af613fcf982833c7239d83c2c16e33
Sean Buckley <sean.bck@gmail.com>
2020-04-24 01:38:04 -0400

    docs

--- docs/single.js
+++ docs/single.js
@@ -28,6 +28,51 @@ function unhideElements(elems) {
         document.getElementById(c).style.display = '';
 }
 
+function insertChallengeCards(targetElem, userData) {
+    let challengeDetails = function (challenge) {
+        let mod = document.createElement('div');
+        mod.classList.add('modalBG');
+        mod.addEventListener('click', function (e) {
+            if (e.target === this || e.target.classList.contains('modalClose'))
+                document.body.removeChild(this);
+        });
+        mod.innerHTML = `<div class="modalBox">
+                            <div class="modalClose">&#10060;</div>
+                            <div style="margin:10px">
+                                 <h3>${challenge.title}</h3>
+                                 ${challenge.text}<br><br>
+                                 <b>Points</b>: ${challenge.points}<br>
+                                 <b>Solves</b>: ${challenge.solves}<br>
+                            </div>
+                         </div>`;
+        document.body.appendChild(mod);
+    };
+    api('challenges').then(function (j) {
+        let sections = {};
+        for (let chal of j) {
+            if (!sections[chal['category']]) {
+                let sectionTitle = document.createElement('div');
+                sectionTitle.textContent = chal['category'];
+                sectionTitle.style.fontSize = '1.5em';
+                targetElem.appendChild(sectionTitle);
+
+                let sectionDiv = document.createElement('div');
+                sectionDiv.classList.add('challengeSection');
+                sections[chal['category']] = sectionDiv;
+                targetElem.appendChild(sectionDiv);
+            }
+            let card = document.createElement('div');
+            card.classList.add('challengeCard')
+            card.style.backgroundColor =
+                userData.solves.includes(chal._id) ? "#28a745" : "#dc3545";
+            card.innerHTML = `<br><b>${chal.title}</b>
+                               <br>${chal.points} points<br><br>`;
+            card.onclick = () => challengeDetails(chal);
+            sections[chal['category']].appendChild(card);
+        }
+    }).catch(loadingError);
+}
+
 window.addEventListener('load', function () {
 
     if (!localStorage.getItem('sessionToken')) {
@@ -37,7 +82,7 @@ window.addEventListener('load', function () {
         return;
     }
 
-    api('userinfo').then(function (x) {
+    api('myuserinfo').then(function (x) {
         userinfo = x;
 
         unhideElements(userinfo.username ?
